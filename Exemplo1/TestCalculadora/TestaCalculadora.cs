3f2765910e5d39c3698afe679671526e2816e73b
Hagnar <Hagnar@DESKTOP-2KERG0O>
2019-06-29 22:31:48 -0300

    Criada a Classe TestaCalculadora para realizar o teste da calsse calculadora e está correto

---
+++ Exemplo1/TestCalculadora/TestaCalculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Exemplo1;
+
+namespace TestCalculadora
+{
+    [TestClass]
+    public class TestaCalculadora
+    {
+        [TestMethod]
+        public void TestaSoma()
+        {
+            Calculadora c = new Calculadora();
+            int resultado = c.Soma(7, 10);
+            Assert.AreEqual(17, resultado);
+        }
+    }
+}
